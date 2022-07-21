151cca3dac99777e667585bd09f94bb3b4c74de2
shio andghuladze <thegwfg@gmail.com>
2022-07-21 10:07:49 +0400

    Initial Commit

---
+++ HM 19 (shio andghuladze)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM 19 (shio andghuladze)
+//
+//  Created by shio andghuladze on 21.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
